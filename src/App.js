e5a08e2f17832d274a1b3104ff972e9f1b60c58f
Esther Schuitemaker <esther252000@gmail.com>
2022-09-28 18:56:50 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import NavBar from './components/navigation/NavBar.jsx';
-import Footer from './components/Footer.jsx';
-import { GlobalStyle } from './styles/globalStyles.js';
-import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <NavBar />
-      <Outlet />
-      <Footer />
-    </>
+
   );
 }
 
