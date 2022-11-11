a97016fa9c77a362b65dda55236b2037ffce0ab1
Esther2504 <esther252000@gmail.com>
2022-11-11 16:26:30 +0100

    middleware verwijderd

--- src/utils/store.js
+++ src/utils/store.js
@@ -26,7 +26,6 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 const store = configureStore({
   reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
-  middleware: [thunk],
 });
 
 export default store
